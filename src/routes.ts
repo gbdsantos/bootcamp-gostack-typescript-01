5d19e4ed1b0a3022a399b7cac191883102b350ef
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-04-13 21:34:28 -0300

    :heavy_plus_sign: update: type objects and vectors
    - Typing objects and vectors.

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from 'express'
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'gbsantos.it@gmail.com',
+    password: '123456',
+    techs: [
+      'NodeJS', 'ReactJS', 'React Native',
+      { title: 'Javascript', experience: 100 },
+    ],
+  });
+
   return response.json({ message: 'Hello World' });
 }
